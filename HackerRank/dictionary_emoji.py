a3429d33f9b229780fb6804734400e46d6e28aa1
Atharv Kekare <atharvkekare@gmail.com>
2021-04-25 21:58:42 +0530

    Added new programs

--- HackerRank/dictionary_emoji.py
+++ HackerRank/dictionary_emoji.py
@@ -1,11 +1,16 @@
+def convert_emoji(message):
+    words = message.split(" ")
+    emojis = {
+        ":)": "😊",
+        ":(": "😓",
+        ";)": "😉"
+    }
+    output_message = ""
+    for word in words:
+        output_message += emojis.get(word, word) + " "
+    return output_message
+
+
 message = input(">")
-words = message.split(" ")
-emojis = {
-    ":)": "😊",
-    ":(": "😓",
-    ";)": "😉"
-}
-output_message = ""
-for word in words:
-    output_message += emojis.get(word, word) + " "
+output_message = convert_emoji(message)
 print(output_message)
